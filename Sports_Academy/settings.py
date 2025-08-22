7d83f4885fd75d0b38e4fa98969dc7f6c8bbf5a2
anshumaaan3 <anshumaanssingh@gmail.com>
2025-08-22 11:42:35 +0530

    Update settings.py

--- Sports_Academy/settings.py
+++ Sports_Academy/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-i*evz$l$fto!b1n5@)uh+t6n*2q22w-$i_rlj2ep8*hlv1creh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
